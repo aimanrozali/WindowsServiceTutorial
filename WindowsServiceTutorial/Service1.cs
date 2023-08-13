3a65008e1034d7ef756156ba34d0a107dd7a2619
aimanrozali <aimanrozali@student.usm.my>
2023-08-14 04:02:33 +0800

    Add check and disable

--- WindowsServiceTutorial/Service1.cs
+++ WindowsServiceTutorial/Service1.cs
@@ -10,11 +10,16 @@ using System.Threading.Tasks;
 using System.Timers;
 using System.IO;
 using System.Management;
+using System.Linq.Expressions;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace WindowsServiceTutorial
 {
     public partial class Service1 : ServiceBase
     {
+        private const string ConnectionString = "Data Source=ARZ\\SQLEXPRESS;Initial Catalog=USB;Integrated Security=True;";
+        private const string AuthorizationQuery = "SELECT COUNT(*) FROM [USB].[dbo].[UsbDevices] WHERE DeviceID = @DeviceID";
         Timer timer = new Timer();
         public Service1()
         {
@@ -56,20 +61,66 @@ namespace WindowsServiceTutorial
                     ManagementBaseObject UWFEnable = item.InvokeMethod("Disable", null, null);
                     WriteToFile(devicename + " ::: " + UWFEnable.ToString() + "::: Disabled");
                 }
+                /*string cmdarg = "/disable-device \"" + devicename + "\""; 
+                initiatePnP(cmdarg);*/
             }
             catch(Exception ex)
             {
                 WriteToFile("Error: " + ex.Message);
             }
         }
+
+        /*private void initiatePnP (string cmd)
+        {
+            try
+            {
+                WriteToFile("Starting CMD..");
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = false,
+                    FileName = "cmd.exe",
+                    RedirectStandardError = false,
+                    //RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    Arguments = $"/C pnputil.exe {cmd}"
+                };
+                WriteToFile(p.StartInfo.Arguments);
+                p.Start();
+                WriteToFile("Executed 1 !");
+                WriteToFile("OUTPUT CMD: "+ p.StandardOutput.ReadToEnd());
+                p.WaitForExit();
+                WriteToFile("ExitCode: " + p.ExitCode);
+                *//*var newProcessInfo = new System.Diagnostics.ProcessStartInfo();
+                newProcessInfo.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+                newProcessInfo.Verb = "runas";
+                newProcessInfo.Arguments = string.Format("pnputil {0}", cmd);
+                WriteToFile("Command: " +  newProcessInfo.Arguments);
+                System.Diagnostics.Process.Start(newProcessInfo);*//*
+            }
+            catch (Exception ex)
+            {
+                WriteToFile ("Exception CMD: "+ex.ToString());
+            }
+            
+        }*/
         private void enableUSB(string devicename)
         {
-            ManagementObjectSearcher myDevices = new ManagementObjectSearcher("root\\CIMV2", @"SELECT * FROM Win32_PnPEntity where Name Like " + '"' + devicename + '"');
-
-            foreach (ManagementObject item in myDevices.Get())
+            try
             {
+                ManagementObjectSearcher myDevices = new ManagementObjectSearcher("root\\CIMV2", @"SELECT * FROM Win32_PnPEntity WHERE DeviceID LIKE '%" + devicename + "%'");
 
-                ManagementBaseObject UWFEnable = item.InvokeMethod("Enable", null, null);
+                foreach (ManagementObject item in myDevices.Get())
+                {
+                    WriteToFile("Enable DeviceID: " + (string)item["DeviceID"]);
+                    ManagementBaseObject UWFEnable = item.InvokeMethod("Enable", null, null);
+                    WriteToFile(devicename + " ::: " + UWFEnable.ToString() + "::: Enabled");
+                }
+            }
+            catch(Exception ex)
+            {
+                WriteToFile("Error: " + ex.Message);
             }
         }
 
@@ -84,14 +135,54 @@ namespace WindowsServiceTutorial
                 if (property.Name == "DeviceID"){
                     
                     devname = property.Value.ToString();
-                    string[] deviceid = devname.Split('\\');
-                    disableUSB(deviceid[2]);
+                    if(CheckUSB(devname))
+                    {
+                        WriteToFile("Authorized");
+                        string[] deviceid = devname.Split('\\');
+                        enableUSB(deviceid[2]);
+                    }
+                    else
+                    {
+                        WriteToFile("Unauthorized");
+                        string[] deviceid = devname.Split('\\');
+                        disableUSB(deviceid[2]);
+                    }
+                    
+                    //WriteToFile(deviceid[2]);
+                    //disableUSB(deviceid[2]);
+                    //enableUSB(deviceid[2]);
 
                     WriteToFile("------------------ START -------------------");
                     WriteToFile(property.Name + " = " + property.Value);
                 }
                 }
             
+        }
+        private bool CheckUSB (string device)
+        {
+            bool isAuthorize = false;
+            
+            try
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(AuthorizationQuery,connection))
+                    {
+                        command.Parameters.AddWithValue("@DeviceID", device);
+                        var count = (int)command.ExecuteScalar();
+                        if (count > 0)
+                            isAuthorize = true;
+                        else
+                            isAuthorize = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Database Exception: "+ ex.Message);
+            }
+            return isAuthorize;
         }
 
        /* private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
